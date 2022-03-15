deb698fbf93d944b96c76e71760ddf421074d9fa
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-15 14:04:56 -0500

    se crean funciones simples para sumar y restar

---
+++ src/Operation.js
@@ -0,0 +1,15 @@
+const sumar = (num1, num2) => {
+    return parseInt(num1) + parseInt(num2)
+}
+
+const restar = (num1, num2) => {
+    return parseInt(num1) - parseInt(num2)
+}
+
+
+
+/*
+exports.module = sumar;
+exports.module = restar;
+exports.module = multPos;
+exports.module = divPos;*/
